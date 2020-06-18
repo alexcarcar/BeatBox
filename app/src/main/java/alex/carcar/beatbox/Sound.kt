49c6c679b8b3317a5a1c642b72d360aea6542180
acarcar <alexcarcar@yahoo.com>
2020-06-17 21:22:23 -0700

    Chapter 20: Unit Testing and Audio Playback

--- app/src/main/java/alex/carcar/beatbox/Sound.kt
+++ app/src/main/java/alex/carcar/beatbox/Sound.kt
@@ -2,6 +2,6 @@ package alex.carcar.beatbox
 
 private const val WAV = ".wav"
 
-class Sound(val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
     val name = assetPath.split("/").last().removePrefix(WAV)
 }
